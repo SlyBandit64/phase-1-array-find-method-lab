f1cf99b72afb2837b7e42c7062d4e2b49866393a
Scott Taylor <rscott.taylor01@gmail.com>
2023-07-19 20:31:01 -0500

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+]
+
+function superbowlWin (array) {
+    const winsRecord = array.find((obj) => {
+        if (obj.result === 'W') {
+            return true
+        }});
+    return winsRecord ? winsRecord.year : undefined;
+}
